d3255b6d03de22880e6783351ba26ee1355e5e99
Mangpha <8350130@gmail.com>
2023-01-11 17:12:39 +0900

    Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,87 @@ class App extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
-          body: Padding(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Hey, Mangpha",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w600,
-                          ),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hey, Mangpha",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
                         ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            fontSize: 18,
-                          ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 20,
                 ),
-              ],
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 20),
-          )),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "\$5 489 149",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber.shade600,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                      ))
+                ],
+              )
+            ],
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 40),
+        ),
+      ),
     );
   }
 }
